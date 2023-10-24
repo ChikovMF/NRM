853af54c202b6fff11e0dbb027915c75f4017139
Чиков М.Ф <ia-matvey@mail.ru>
2023-10-24 17:18:07 +0300

    24.10.2023 fix

---
+++ NRM/obj/Release/net7.0/PubTmp/Out/wwwroot/lib/chart/myChart.js
@@ -0,0 +1,12 @@
+﻿const ctx = document.getElementById('chart1');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: data1.map(row => row.label),
+        datasets: [{
+            label: 'Количество посылок направляющихся к местам',
+            data: data1.map(row => row.count)
+        }]
+    },
+});
