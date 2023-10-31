13589d34a03bb5ee4022e51650112acfff1654fc
Чиков М.Ф <ia-matvey@mail.ru>
2023-10-31 17:00:00 +0300

    Доступ к редактированию гр РПО

--- NRM/Pages/AdminPanel/GroupParcel/Edit.cshtml.cs
+++ NRM/Pages/AdminPanel/GroupParcel/Edit.cshtml.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NRM.Services;
 
 namespace NRM.Pages.AdminPanel.GroupParcel
 {
+    [Authorize(Roles = "Полный администратор")]
     public class EditModel : PageModel
     {
         private readonly GroupParcelService _groupParcelService;
