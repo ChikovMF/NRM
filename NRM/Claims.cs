8d557dc70e7c2887305a4e545f3f8d7b6097277a
SnOOb <snoob@bk.ru>
2023-11-20 14:53:15 +0300

    Добавлены элементы в "navbar", добавлена иконка приложения, добавлен вывод логина пользователя рядом с кнопкой "Выйти", пеереименован администратор пользователей в администратор узла

--- NRM/Claims.cs
+++ NRM/Claims.cs
@@ -3,7 +3,7 @@
     public class Claims
     {
         public const string FullAccessEnter = "Полный администратор";
-        public const string UserAdministratorEnter = "Администратор пользователей";
+        public const string UserAdministratorEnter = "Администратор узла";
         public const string ParcelAdministratorEnter = "Администратор посылок";
         public const string UserEnter = "Пользователь";
     }
