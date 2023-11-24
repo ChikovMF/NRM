938bf5986725cbbb1576107fa35ea542690d63f5
Чиков М.Ф <ia-matvey@mail.ru>
2023-11-24 13:19:16 +0300

    В состав группы РПО добалаются все несгруппированные отправления, у которых схожее место доставки.

---
+++ NRM/Controllers/ParcelsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace NRM.Controllers
+{
+    public class ParcelsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ParcelsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("api/[controller]/GetUnused")]
+        public async Task<List<Item>> GetUnusedParcels(int? placeId)
+        {
+            return await _context.Parcels
+                .Where(p => p.PlaceOfDeliveryId == placeId || p.PlaceOfDeliveryId == null)
+                .Where(p => p.GroupParcelId == null)
+                .Select(p => new Item
+                {
+                    Text = p.TrackNumber,
+                    Value = p.Id
+                })
+                .ToListAsync();
+        }
+
+        public class Item
+        {
+            public int Value { get; set; }
+            public string Text { get; set; } = null!;
+        }
+    }
+}
